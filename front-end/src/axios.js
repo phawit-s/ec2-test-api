c986c13ac16260fb7bc23fa05794e5e7ca6c29cd
macsakarn <macsakarn@gmail.com>
2022-05-04 02:10:41 +0700

    .

--- front-end/src/axios.js
+++ front-end/src/axios.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 export const HTTP = axios.create({
-baseURL: 'http://ec2-13-214-81-88.ap-southeast-1.compute.amazonaws.com/',
+baseURL: 'http://ip-10-0-142-187.ap-southeast-1.compute.internal/',
 });
