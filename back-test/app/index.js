9331e27aebe15b3fe7a960c04ae4755adae660f1
macsakarn <macsakarn@gmail.com>
2022-05-04 02:50:34 +0700

    .

--- back-test/app/index.js
+++ back-test/app/index.js
@@ -2,7 +2,13 @@ var express = require('express')
 var cors = require('cors')
 var app = express()
 
-app.use(cors({credentials: true, origin: "http://13.213.140.233"}))
+app.use(cors({credentials: true}))
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "ec2-**-***-***-**.ap-southeast-1.compute.amazonaws.com"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', (req, res) => {
     res.json({text:"Hello Backend"})
